305ce4887f32bc0b03e3584af6594c5d1a4c69e3
Bereket Kinfe <112067722+Bekione@users.noreply.github.com>
2023-06-06 13:54:51 +0300

    Update main.js add comments

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,26 @@
+//Calling setClock method so that it updates the position of each hand(second, minute, hour) every 1 second.
 setInterval(setClock, 1000);
 
+//Selecting the three hands by their data attribute
 const hourHand = document.querySelector("[data-hour-hand]");
 const minuteHand = document.querySelector("[data-minute-hand]");
 const secondHand = document.querySelector("[data-second-hand]");
 
 function setClock(){
     let currentTime = new Date();
+    //Calculating the time ratio 
     const secondsRatio = currentTime.getSeconds() / 60;
     const minutesRatio = (secondsRatio + currentTime.getMinutes() ) / 60;
     const hoursRatio = (minutesRatio + currentTime.getHours() ) / 12;
-
+    
+    //Passing the above calculated ratios for setRotation method to change the hands position
     setRotation(hourHand, hoursRatio);
     setRotation(minuteHand, minutesRatio);
     setRotation(secondHand, secondsRatio);
 }
 function setRotation(element, rotationRatio){
-    element.style.setProperty("--rotation", (rotationRatio * 360 + "deg"));
+    //Setting the --rotation css variable with the ratio
+    element.style.setProperty("--rotation", (rotationRatio * 360 + "deg")); 
 }
 
 setClock();
